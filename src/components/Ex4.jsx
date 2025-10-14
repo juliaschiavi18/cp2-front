33754ac1ef92f5c5785a48abe1b6225348d64179
Laboratório FIAP <labsfiap@fiap.com.br>
2025-10-14 20:31:46 -0300

    salvando 2

--- src/components/Ex4.jsx
+++ src/components/Ex4.jsx
@@ -6,7 +6,13 @@ export default function Ex4() {
   return (
     <div className="my-8">
         <h1 className="text-2xl font-bold mb-4">Ex4 - Card Responsivo</h1>
-      {/* Resolva aqui */}
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6">
+        <div className="bg-white p-6 rounded shadow hover:scale-105 transition-transform">
+          <h3 className="text-[clamp(1rem,2vw,1.5rem)] font-semibold mb-2">Titulo</h3>
+          <p className="text-gray-600">texto do card.</p>
+          <button className="mt-4 px-4 py-2 bg-pink-600 text-white rounded hover:bg-pink-700">Botao</button>
+        </div>
+    </div>
     </div>
   );
 }
--- src/components/Ex5.jsx
+++ src/components/Ex5.jsx
@@ -9,7 +9,22 @@ export default function Ex5() {
   return (
     <div className="my-8">
         <h1 className="text-2xl font-bold mb-4">Ex5 - Navbar Responsiva</h1>
-      {/* Resolva aqui */}
+       <header className="bg-red-800 text-white p-4">
+      <nav className="flex flex-col md:flex-row md:items-center md:justify-between">
+        <h1 className="text-xl font-bold">Bem vindo</h1>
+        <ul className="flex flex-col md:flex-row gap-4 mt-4 md:mt-0">
+          <li className="relative after:content-[''] after:block after:h-[2px] after:bg-blue-400 after:scale-x-0 after:transition-transform after:duration-300 hover:after:scale-x-100">
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#" className="animate-blink-link">Promoções</a>
+          </li>
+          <li className="relative after:content-[''] after:block after:h-[2px] after:bg-blue-400 after:scale-x-0 after:transition-transform after:duration-300 hover:after:scale-x-100">
+            <a href="#">Contato</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
     </div>
   );
 }
