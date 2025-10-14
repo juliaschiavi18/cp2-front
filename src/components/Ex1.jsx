c6662fe28874e8bf70ada8f20009bd0467715c8f
Laboratório FIAP <labsfiap@fiap.com.br>
2025-10-14 20:01:55 -0300

    exercicio 1

--- src/components/Ex1.jsx
+++ src/components/Ex1.jsx
@@ -6,7 +6,13 @@ export default function Ex1() {
   return (
     <div className="my-8">
         <h1 className="text-2xl font-bold mb-4">Ex1 - Botão Estilizado</h1>
-      {/* Resolva aqui */}
+        <button class="rounded-md bg-blue-600 py-2 px-4 border border-transparent 
+        text-center text-sm text-white transition-all shadow-md 
+        hover:shadow-lg focus:bg-blue-700 focus:shadow-none
+         active:bg-blue-700 hover:bg-blue-700 active:shadow-none 
+         disabled:pointer-events-none  " 
+         type="button">Botao</button>
     </div>
   );
 }
+
