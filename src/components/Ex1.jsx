6e0b561169c636007f59f09ba0ba6b99fb671ded
Laboratório FIAP <labsfiap@fiap.com.br>
2025-10-14 21:11:49 -0300

    salvando

--- src/components/Ex1.jsx
+++ src/components/Ex1.jsx
@@ -3,12 +3,21 @@
 // Crie um botão centralizado com a cor azul, texto branco, padding, bordas arredondadas e efeito `hover` para escurecer a cor.
 
 export default function Ex1() {
-  return (
-    <div className="my-8">
+ return (
       
-        <h1 className="text-2xl font-bold mb-4">Ex1 - Botão Estilizado</h1>
-        <button class="rounded-md bg-blue-600 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-blue-700 focus:shadow-none active:bg-blue-700  hover:bg-blue-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none ml-2">botao</button>
-    </div>
-  );
+      <div>
+        <h1 className="text-2xl font-bold mb-4">Ex2 - Flexbox Básico</h1>
+         <div className="flex flex-row justify-center  ">
+        
+        <button class="rounded-md bg-blue-600 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-blue-700 focus:shadow-none active:bg-blue-700 hover:bg-blue-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none ml-2">botao</button>
+        
+        
+
+      </div>
+     
+      </div>
+     
+   
+  )
 }
 
